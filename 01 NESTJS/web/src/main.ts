1732091c6bfcd9d67332c5b40defb197edda01ac
Adrian Eguez <eadepto@hotmail.com>
2021-08-31 08:53:35 -0500

    Clases

--- 01 NESTJS/web/src/main.ts
+++ 01 NESTJS/web/src/main.ts
@@ -6,3 +6,57 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+// package.json
+// npm run start
+// nodejs command prompt
+
+/// VARIABLES
+// TIPOS DE VARIABLES
+
+// MUTABLES ( reasignar -> = )
+var variableUno = 1; // NO USAMOS VAR !
+let variableDos = 2;
+variableUno = 3;
+variableDos = 4;
+// INMUTABLES ( No se pueden reasignar X -> != )
+const variableTres = 5;
+// variableTres = 2; // ERROR!
+
+// PRIMITIVAS PRIMITIVAS (Typescript)
+
+const texto: string = ''; // "" ``
+const numeroEntero: number = 1;
+const numeroFlotante: number = 1.2;
+const soyEstudiante: boolean = true; // false
+const noDefinido = undefined;
+const noHayNada = null;
+const fecha: Date = new Date();
+// Duck Typing
+const textoDos = 'Adrian';
+let cualquierCosa: any = 'Vicente';
+cualquierCosa = 1;
+cualquierCosa = true;
+cualquierCosa = new Date();
+
+class Usuario {
+  constructor(
+      public nombre: string,
+      public apellido: string
+  ) {
+  }
+}
+const usuario: Usuario = new Usuario('Adrian', 'Eguez');
+usuario.nombre;
+usuario.apellido;
+
+interface UsuarioInterface {
+  nombre: string;
+  apellido: string;
+  edad? : number; // ? => Opcional // Valor por defecto es undefined
+}
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# web-eguez-sarzosa-vicente-adrian
-WEB-APELLIDO_PATERNO-APELLIDO_MATERNO-NOMBRES_COMPLETOS
+# Comandos docker
+
+```
+$ docker run --name web-mysql 
+-e MYSQL_ROOT_PASSWORD=123456789 
+-e MYSQL_DATABASE=web 
+-e MYSQL_USER=epn
+-e MYSQL_PASSWORD=epn12345678 
+-p 3010:3306 -d mysql:5.7
+
+```
