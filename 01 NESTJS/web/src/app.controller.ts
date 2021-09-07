4ba2409a83328dea650a0e05fc3a16d9578b6a59
Adrian <eadepto@hotmail.com>
2021-09-07 10:54:54 -0500

    Cookies

--- 01 NESTJS/web/src/app.controller.ts
+++ 01 NESTJS/web/src/app.controller.ts
@@ -1,11 +1,17 @@
-import {Controller, Get, HttpCode} from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  InternalServerErrorException,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-      private readonly appService: AppService
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
@@ -27,4 +33,43 @@ export class AppController {
   holaJSON(): string {
     return '{mensaje: "Hola json" }';
   }
+
+  @Get('bad-request')
+  badRequest() {
+    throw new BadRequestException();
+  }
+
+  @Get('internal-error')
+  internalError() {
+    throw new InternalServerErrorException();
+  }
+
+  @Get('setear-cookie-insegura')
+  setearCookieInsegura(
+    @Req() req, //  request - PETICION
+    @Res() res, //  response - RESPUESTA
+  ) {
+    res.cookie(
+      'galletaInsegura', // nombre
+      'Tengo hambre', // valor
+    );
+    res.cookie(
+      'galletaSegura', // nombre
+      'Web :3', // valor
+      {
+        secure: true,
+      },
+    );
+    res.send('ok'); // return de antes
+  }
+
+  @Get('mostrar-cookies')
+  mostrarCookies(@Req() req) {
+    const mensaje = {
+      sinFirmar: req.cookies,
+      firmadas: req.signedCookies,
+    };
+    return mensaje;
+  }
 }
+// npm i cookie-parser express-session session-file-store
--- 01 NESTJS/web/src/main.ts
+++ 01 NESTJS/web/src/main.ts
@@ -1,17 +1,31 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+const cookieParser = require('cookie-parser'); // Importar cosas en JS
+const express = require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000); // PUERTO
-    // package.json
-    // npm run start
+  const app = await NestFactory.create(AppModule);
+  app.use(express.static('publico')); // Servidor Web Estatico
+  app.use(cookieParser('Me agradan los poliperros')); // Secreto Cookies
+  app.use(// Session
+    session({
+      name: 'server-session-id',
+      secret: 'No sera de tomar un traguito',
+      resave: true,
+      saveUninitialized: true,
+      cookie: { secure: false },
+      store: new FileStore(),
+    }),
+  );
+
+  await app.listen(3000); // PUERTO
+  // package.json
+  // npm run start
 }
 
 bootstrap();
-
-
-
 
 /*
 
@@ -216,10 +230,3 @@ if (undefined) {
 
 
 */
-
-
-
-
-
-
-
