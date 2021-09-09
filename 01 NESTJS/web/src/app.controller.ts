1638c57f6a13e2d81d41209204fb061b7c928d6e
Adrian <eadepto@hotmail.com>
2021-09-09 17:43:58 -0500

    Parametros

--- 01 NESTJS/web/src/app.controller.ts
+++ 01 NESTJS/web/src/app.controller.ts
@@ -1,33 +1,45 @@
 import {
   BadRequestException,
+  Body,
   Controller,
   Get,
+  Header,
+  Headers,
   HttpCode,
   InternalServerErrorException,
+  Param,
+  Post,
+  Query,
   Req,
   Res,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// npm i cookie-parser express-session session-file-store
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
   // import {Controller, Get, HttpCode} from '@nestjs/common';
   @Get('texto')
   @HttpCode(200)
   holaTexto(): string {
     return 'HOLA TEXTO';
   }
+
   @Get('html')
   @HttpCode(201)
   holaHTML(): string {
     return '<h1>Hola HTML</h1> <button>Click</button>';
   }
+
   @Get('json')
   @HttpCode(200)
   holaJSON(): string {
@@ -54,10 +66,11 @@ export class AppController {
       'Tengo hambre', // valor
     );
     res.cookie(
-      'galletaSegura', // nombre
+      'galletaSeguraYFirmada', // nombre
       'Web :3', // valor
       {
-        secure: true,
+        secure: true, // solo se transfiera por canales confiables https
+        signed: true, // Encriptacion
       },
     );
     res.send('ok'); // return de antes
@@ -69,7 +82,33 @@ export class AppController {
       sinFirmar: req.cookies,
       firmadas: req.signedCookies,
     };
+    // req.signedCookies.total
     return mensaje;
   }
+
+  @Get('parametros-consulta/:nombre/:apellido')
+  @HttpCode(200)
+  @Header('Cache-Control', 'none') // Cabeceras de respuesta (response headers)
+  @Header('EPN', 'SISTEMAS') // Cabeceras de respuesta (response headers)
+  parametrosConsulta(
+    @Query() queryParams,
+    @Param() params,
+  ) {
+    return {
+      parametrosConsulta: queryParams,
+      parametrosRuta: params,
+    };
+  }
+
+  @Post('parametros-cuerpo') // 201
+  @HttpCode(200)
+  parametrosCuerpo(
+    @Body() bodyParams,
+    @Headers() cabecerasPeticion,
+  ) {
+    return {
+      parametrosCuerpo: bodyParams,
+      cabeceras: cabecerasPeticion,
+    };
+  }
 }
-// npm i cookie-parser express-session session-file-store
