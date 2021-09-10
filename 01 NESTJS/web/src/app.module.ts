007f49be551ee6cbb5ec3e9fbd1492b46b690ab2
Adrian <eadepto@hotmail.com>
2021-09-10 12:55:22 -0500

    hasta prisma

--- 01 NESTJS/web/src/app.module.ts
+++ 01 NESTJS/web/src/app.module.ts
@@ -1,21 +1,25 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 // DECORADOR -> Funciones
 @Module({
-    imports: [ // Modulos importados
-
-    ],
-    controllers: [ // Controladores de este modulo
-        AppController
-    ],
-    providers: [ // Servicios de este modulo
-        AppService
-    ],
-    exports: [ // Servicios EXPORTADOS (que se pueden usar fuera de este modulo)
-        AppService
-    ],
+  imports: [
+    // Modulos importados
+  ],
+  controllers: [
+    // Controladores de este modulo
+    AppController,
+  ],
+  providers: [
+    // Servicios de este modulo
+    AppService,
+    PrismaService,
+  ],
+  exports: [
+    // Servicios EXPORTADOS (que se pueden usar fuera de este modulo)
+    AppService,
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}
