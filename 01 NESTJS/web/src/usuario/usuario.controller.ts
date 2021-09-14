6c8c129c39d86dbd9e84b26a400d210492b9c293
Adrian <eadepto@hotmail.com>
2021-09-14 09:30:14 -0500

    explicacion de servicio

--- 01 NESTJS/web/src/usuario/usuario.controller.ts
+++ 01 NESTJS/web/src/usuario/usuario.controller.ts
@@ -11,6 +11,20 @@ export class UsuarioController {
 
   @Get(':idUsuario')
   obtenerUno(@Param() parametrosRuta) {
+    this.usuarioService.crearUno({
+      apellido: '...',
+      fechaCreacion: new Date(),
+      nombre: '...',
+    });
+    this.usuarioService.actualizarUno({
+      id: 1,
+      data: {
+        nombre: '...',
+        // fechaCreacion: '...',
+        // fechaCreacion: new Date(),
+      },
+    });
+    this.usuarioService.eliminarUno(1);
     return this.usuarioService.buscarUno(+parametrosRuta.idUsuario);
   }
 }
--- 01 NESTJS/web/src/usuario/usuario.service.ts
+++ 01 NESTJS/web/src/usuario/usuario.service.ts
@@ -7,8 +7,7 @@ export class UsuarioService {
   constructor(
     // Inyectar dependencias
     private prisma: PrismaService,
-  ) {
-  }
+  ) {}
 
   buscarUno(id: number) {
     return this.prisma.ePN_USUARIO.findUnique({
@@ -25,18 +24,20 @@ export class UsuarioService {
   }
 
   actualizarUno(parametrosActualizar: {
-    where: Prisma.EPN_USUARIOWhereUniqueInput;
+    id: number;
     data: Prisma.EPN_USUARIOUpdateInput;
   }) {
     return this.prisma.ePN_USUARIO.update({
       data: parametrosActualizar.data,
-      where: parametrosActualizar.where,
+      where: {
+        id: parametrosActualizar.id,
+      },
     });
   }
 
-  eliminarUno(where: Prisma.EPN_USUARIOWhereUniqueInput) {
+  eliminarUno(id: number) {
     return this.prisma.ePN_USUARIO.delete({
-      where: where,
+      where: { id: id },
     });
   }
 }
